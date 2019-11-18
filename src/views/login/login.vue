ca32a6750df6ca73ab2c1790acb8eea154639080
StarALiang <1098318460@qq.com>
2019-11-18 18:13:05 +0800

    登录页提交

---
+++ src/views/login/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <div class="ms-login">
+      <div class="ms-title">后台管理系统</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  background-image: url(../../assets/img/login-bg.jpg);
+  background-size: 100%;
+  position: relative;
+  .ms-login {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 350px;
+    margin: -190px 0 0 -175px;
+    border-radius: 5px;
+    background: rgba(255, 255, 255, .3);
+    overflow: hidden;
+    .ms-title {
+      width: 100%;
+      line-height: 50px;
+      text-align: center;
+      font-size: 20px;
+      color: #fff;
+      border-bottom: 1px solid #ddd;
+    }
+  }
+}
+</style>
